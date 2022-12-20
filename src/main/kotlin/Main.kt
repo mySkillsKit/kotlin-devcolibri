c9fb981985f7a7897b9ec71de79aa133feb6d91d
Anatolii Vasilev <avas2012@gmail.com>
2022-12-20 20:59:26 +0300

    part 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,5 +13,17 @@ fun main(args: Array<String>) {
 
     val userKotlin = UserKotlin("anat@gmail.com")
     println("UserKotlin ${userKotlin.email}")
+    var a1: String = "123"
+    var a6: Byte = 12 //8 bit
+    var a0: Short = 11 //16 bit
+    var a2: Int = 123  //32 bit
+    var a3: Long = 123L //64 bit
+    var a4: Double = 1.1  //64 bit
+    var a5: Float = 1.33F //32 bit
+
+    var b1: Long = a2.toLong()
+
+    var c1: Char = 'a'
+    var bool: Boolean = true
 
 }
